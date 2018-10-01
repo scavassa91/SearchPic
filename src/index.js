c8aac4ed2f3032d57dae4132d057ec1bb5954257
scavassa91 <dan.scavassa@gmail.com>
2018-10-01 19:56:16 -0300

    Added redux into the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import './styles/index.css';
+import App from './components/App';
+import reducers from './reducers';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
